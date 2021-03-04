69535253cb673ae05ac47f25c573c1295e069d51
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-03-04 13:46:39 -0500

    add run validators, trim to comment and requires to reply schema

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -39,9 +39,9 @@ const pizzaController = {
       .catch(err => res.json(err));
   },
 
-  // update pizza by id
-  updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+// update pizza by id
+updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
@@ -49,7 +49,7 @@ const pizzaController = {
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.json(err));
+      .catch(err => res.status(400).json(err));
   },
 
   // delete pizza
